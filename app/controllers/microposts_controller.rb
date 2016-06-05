e0ef70dd95caf16369fe19a7101a2c7fe92e0354
lukaswet <lukaswet@gmail.com>
2016-06-05 07:26:37 +0000

    FIxed images showing in microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
-
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -12,21 +12,17 @@ class MicropostsController < ApplicationController
       render 'static_pages/home'
     end
   end
-
+  
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || root_url
   end
-
-  private
   
+  private
+    
     def micropost_params
       params.require(:micropost).permit(:content, :picture)
-    end
-
-    def micropost_params
-      params.require(:micropost).permit(:content)
     end
     
     def correct_user
